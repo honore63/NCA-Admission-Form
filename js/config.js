90a28c56f8ad765a72b7eb053738e61bba3d1da6
TUYISHIME HONORE <54102325012@reb.ac.rw>
2026-07-27 18:45:40 +0300

    Update Supabase credentials in config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://ilmtgzejzgewbfqsfdqd.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_1zi3-9gsMc7-O8wII_6tnw_4aNhHU-l";
+const SUPABASE_URL = "https://faajngoklmoydbzyjxpz.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhYWpuZ29rbG1veWRienlqeHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNjE5ODksImV4cCI6MjEwMDczNzk4OX0.C3SfjHSNdOYrhjU2jQmNUalG8JWf8P2e09p-D0XrYhg";
 
 var supabase = null;
 
